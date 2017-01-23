2e68bf2a7171614b3109d849b26e4be75a76e42f
Tinetok <nicolas.vanherpen@isep.fr>
2017-01-23 21:47:09 +0100

    ez

--- fonction/ajout_fonction.php
+++ fonction/ajout_fonction.php
@@ -2,25 +2,47 @@
 //définition des variables permettant de se connecter au serveur
 	$servername = "localhost";
 	$username = "root";
-	$password = "";
+	$password = "root";
 	$dbname = "mydb";
+//récupération de idPièce à partir de l'URL
+	$idPiece=$_GET['id'];
+	$NomFonctionnalite = $_POST['Fonctionnalite'];
+	$CleProduit = $_POST['CleProduit'];
+
 //on tente de se connecter à la base de données
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 //Execution de la requette vers la base de données
-	$sql = "INSERT INTO Fonctionnalite (NomFonctionnalite, CleProduit) WHERE idPiece="?"
-VALUES ('{$_POST['NomFonctionnalite']}','{$_POST['CleProduit']}','{$_POST['Mail']}','{$_POST['Mdp']}','{$_POST['Rue']}','{$_POST['Numero']}','{$_POST['CodePostal']}')";
+	if(!empty($NomFonctionnalite) && !empty($CleProduit))
 
-//check s'il y a erreur ou non 
-if (mysqli_query($conn, $sql)) {
-    echo "Félicitation kamarade";
-} 
-//si erreur indique l'erreur, coupe la connexion
-else 
-{
-    echo "Error :'( " . $sql . "<br>" . mysqli_error($conn);
-}
-//ferme la connection
-mysqli_close($conn);
+	{
+
+		$sql = "INSERT INTO Fonctionnalite (NomFonctionnalite, CleProduit)
+		VALUES ('$NomFonctionnalite','$CleProduit')";
+		mysqli_execute($sql);
+
+
+
+		// affiche si il y a eu une erreur ou non: si pas d'erreur
+		// Vérification des erreur
+		if (mysqli_query($conn, $sql)) 
+			{
+		    	echo "Fonction ajouter";
+			} 
+		//si erreur indique l'erreur et tue la connectio
+		else 
+			{
+		    		echo "Error :'( " . $sql . "<br>" . mysqli_error($conn);
+			}
+		//fermer la connection
+		mysqli_close($conn);
+	}
+else
+	{
+	echo "Veuilliez saisir tous les champs du formulaire";
+	}
+
+
+
 
 
 
